7efdb5bc4d73bf07c8b1dabedd774e176540f8a2
Gareth <hello@gareth.world>
2024-05-03 12:25:30 +0100

    more lighthouse fixes

--- src/js/insta.js
+++ src/js/insta.js
@@ -8,7 +8,7 @@ window.insta = function () {
           'IGQWROWFhaMFExUUZA4VXYtbkt1SnFVR2gyVENySWxzQk8tSEtOYnFrNXdSS0JDRlVSbk9hRlRCYkVLOHFtLXY2Uno0aTA1NENaa0JCWmR1U2toZAm1hSmt4eEhwcFNVRms1S2VpTEhrbW4wTk1fUGVxX3lHUHJVcG8ZD',
         limit: 4,
         template:
-          '<a aria-label="instagram post" class="block w-full" href="{{link}}"><img class="w-full" title="{{caption}}" src="{{image}}" alt="{{caption}}" /></a>',
+          '<a aria-label="instagram post" class="block w-full" href="{{link}}"><img class="w-full" title="{{caption}}" src="{{image}}" alt="{{caption}}" loading="lazy" /></a>',
       });
 
       feed.run();
